bb0f90acb65eafa2b2e7ef5d9cab8d36f313190b
reggybuyer <youremail@example.com>
2026-03-02 16:11:19 +0300

    fix component import path

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import UserLogin from "./components/UserLogin";
-import User from "./components/User";
 import UserRegister from "./components/UserRegister";
-import Admin from "./components/admin";
 import AdminLogin from "./components/AdminLogin";
+import User from "./components/user";
+import Admin from "./components/admin";
+
 import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<UserLogin />} />
       <Route path="/login" element={<UserLogin />} />
       <Route path="/register" element={<UserRegister />} />
-      <Route path="/user" element={<User />} />
 
-      {/* Admin */}
       <Route path="/admin-login" element={<AdminLogin />} />
       <Route path="/admin" element={<Admin />} />
+
+      <Route path="/user" element={<User />} />
     </Routes>
   </BrowserRouter>
 ); 
